6f0f594e097f410dacdfbb470d621c40bcd06550
tsunami99-han <doanhongson050999@gmail.com>
2021-05-25 08:34:04 +0700

    BubbleSort

---
+++ Bai1/src/BubbleSort.java
@@ -0,0 +1,25 @@
+public class BubbleSort {
+    static int[] list={2,4,6,8,1,3,3,5,7,9,0,13};
+
+    public static void bubbleSort(int[] list) {
+        boolean needNextPass = true;
+
+        for (int k = 1; k < list.length && needNextPass; k++) {
+            needNextPass = false;
+            for (int i = 0; i < list.length - k; i++) {
+                if (list[i] > list[i + 1]) {
+                    int temp = list[i];
+                    list[i] = list[i + 1];
+                    list[i + 1] = temp;
+
+                    needNextPass = true;
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        bubbleSort(list);
+        for (int i = 0; i < list.length; i++)
+            System.out.print(list[i] + " ");
+    }
+}
